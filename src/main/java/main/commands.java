6628ec09b765574f4768bbd8b52985fa331a731b
wyatt_was <wyatt.ruehl@gmail.com>
2023-11-02 19:25:28 +0100

    updating project to current status

--- src/main/java/main/commands.java
+++ src/main/java/main/commands.java
@@ -14,69 +14,25 @@ import javax.annotation.Nonnull;
 
 public class commands extends ListenerAdapter {
 
-    private String getActivities(List activitiesList) {
-        String activitie = "";
-        if (!activitiesList.isEmpty()) {
-            Activity tempActiv = (Activity) activitiesList.get(0);
-            for (int i = 1; i < activitiesList.size(); i++) {
-                tempActiv = (Activity) activitiesList.get(i);
-                activitie = activitie + ", " + tempActiv;
-            }
-        } else {
-            activitie = "No activitie";
-        }
-        return activitie;
-    }
-
-    private String getRolesAsString(List rolesList) {
-        String roles = "";
-        if (!rolesList.isEmpty()) {
-            Role tempRole = (Role) rolesList.get(0);
-            roles = tempRole.getAsMention();
-            for (int i = 1; i < rolesList.size(); i++) {
-                tempRole = (Role) rolesList.get(i);
-                roles = roles + ", " + tempRole.getAsMention();
-            }
-        } else {
-            roles = "No roles";
-        }
-        return roles;
-    }
-
     @Override
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
         String command = event.getName();
 
-        if (command.equals("help")) {
-            EmbedBuilder emb = new EmbedBuilder();
-
-            emb.setTitle("LXBS Commands")
-                    .setDescription("List of all available commands")
-                    .setColor(0x828282)
-                    .setThumbnail(
-                            "https://cdn.discordapp.com/attachments/837779743486378075/1125779362919219252/lxbs-logo-meme-alex-jones.png")
-                    .addField("</respect:1125414321204236334>", "Pay respect", true)
-                    .addField("</rickroll:1125414321204236333>", "Rick-roll someone", true)
-                    .addField("", "", false)
-                    .addField("More Infos: ", "[lxbs.online](https://lxbs.online)", false);
-
-            event.replyEmbeds(emb.build())
-                    .addActionRow(Button.link("http://lxbs.online", "lxbs.online"))
-                    .setEphemeral(true)
-                    .queue();
-        }
-
         if (command.equals("rickroll")) {
             Member member = event.getOption("user").getAsMember();
-            event.reply(member.getAsMention()
-                    + "\n https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713")
-                    .queue();
+            EmbedBuilder emb = new EmbedBuilder();
+            emb.setTitle("You just got rickrolled!", "https://youtube.com/watch?=dQw4w9WgXcQ")
+                .setAuthor("Rick Astley", "https://youtu.be/dQw4w9WgXcQ")
+                .setDescription(member.getAsMention() + " well, not as you thought it was going..." + <meta>)
+            event.replyEmbeds(emb.build()).queue();
         }
 
         if (command.equals("respect")) {
             Member member = event.getOption("user").getAsMember();
-            event.reply(member.getAsMention()
-                    + "\n https://tenor.com/view/keyboard-hyperx-rgb-hyperx-family-hyperx-gaming-gif-17743649").queue();
+            EmbedBuilder emb = new EmbedBuilder();
+            emb.setDescription(member.getAsMention())
+                .setImage("https://tenor.com/view/keyboard-hyperx-rgb-hyperx-family-hyperx-gaming-gif-17743649");
+            event.replyEmbeds(emb.build()).queue();
         }
 
         if (command.equals("leave")) {
